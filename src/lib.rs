7c920f8d7a059822ca2ab91e05173523dc2cf605
nas <nas.silver.bullet@gmail.com>
2020-11-24 01:18:09 +0900

    fix lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,53 @@
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
+use yew::web_sys::HtmlInputElement;
 
 struct Model {
     link: ComponentLink<Self>,
-    value: i64,
+    state: State,
+    input_ref: NodeRef,
+}
+
+pub struct State {
+    pub value: String,
+    pub old: Vec<String>,
 }
 
 enum Msg {
-    AddOne,
+    Update(String),
+    Key(String),
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        let state = State {
+            value: "".into(),
+            old: vec![],
+        };
+
+        let input_ref = NodeRef::default();
+
+        Self {
+            link,
+            state,
+            input_ref,
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1,
+            Msg::Update(val) => {
+                self.state.value = val;
+            }
+            Msg::Key(val) => self.check_key(val),
         }
+
+        if let Some(input) = self.input_ref.cast::<HtmlInputElement>() {
+            input.focus().unwrap();
+        }
+
         true
     }
 
@@ -31,13 +58,50 @@ impl Component for Model {
         false
     }
 
+    fn rendered(&mut self, _: bool) {
+        if let Some(input) = self.input_ref.cast::<HtmlInputElement>() {
+            input.focus().unwrap();
+        }
+    }
+
     fn view(&self) -> Html {
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <ul class="item-list" style="list-style: none; font-size: 12px; font-family: 'Microsoft Sans Serif'">
+                    { for self.state.old.iter().enumerate().map(|e| self.view_entry(e))}
+                    {">"}
+                    <input
+                        ref=self.input_ref.clone()
+                        style="border: none; background: none; outline: none; padding: 0px; font-size: 12px; font-family"
+                        class=""
+                        value=&self.state.value
+                        oninput=self.link.callback(|e: InputData| Msg::Update(e.value))
+                        onkeypress=self.link.callback(|e: KeyboardEvent| Msg::Key(e.key()))
+                    />
+                </ul>
             </div>
         }
+    }
+}
+
+impl Model {
+    fn view_entry(&self, (_, entry): (usize, &String)) -> Html {
+        html! {
+            <>
+                <li style="list-style: none; font-size: 12px; font-family: 'Microsoft Sans Serif'">
+                { format!{"{}{}",">",  entry} }
+                </li>
+            </>
+        }
+    }
+
+    fn check_key(&mut self, val: String) {
+        if val != "Enter" {
+            return;
+        }
+
+        self.state.old.push(self.state.value.to_string());
+        self.state.value = "".into();
     }
 }
 
